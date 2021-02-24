964a9d7ad68137f47a624f16ab216c4d2ff778ab
kyleriegel <77362035+kyleriegel@users.noreply.github.com>
2021-02-24 00:02:07 -0500

    fixes

--- ScavengerFunFinal/Assets/PauseController.cs
+++ ScavengerFunFinal/Assets/PauseController.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PauseController : MonoBehaviour {
 
     public GameObject pausePanel;
+    public Text endGame;
 
     public void PauseGame()  {
         pausePanel.SetActive(true);
@@ -23,9 +25,12 @@ public class PauseController : MonoBehaviour {
     {
         GameObject timer = GameObject.Find("Timer");
         TimerScript timerScript = timer.GetComponent<TimerScript>();
-        if (timerScript.currentTime <= 0)
+        if (timerScript.currentTime <= 0 && !TimerScript.gameWon)
         {
-            
+            pausePanel.SetActive(true);          
+            endGame = GameObject.Find("EndGame").GetComponent<Text>();
+            endGame.text = "Try Again";
+            Time.timeScale = 0;
         }
     }
 
--- ScavengerFunFinal/Assets/TimerScript.cs
+++ ScavengerFunFinal/Assets/TimerScript.cs
@@ -2,23 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class TimerScript : MonoBehaviour
 {
-    public static bool timerActive = false;
+    public static bool timerActive = true;
     public int maxTime;
     public int numItems;
     public int currentTime;
+    public static bool gameWon;
     public Text timerText;
+    public Text winner;
+
 
     // Start is called before the first frame update
     void Start()
     {
         timerText.text = "Timer: " + maxTime.ToString();
+        currentTime = maxTime;
+    }
+
+    void wait()
+    {
+        winner = GameObject.Find("EndGame").GetComponent<Text>();
+        winner.text = "You Won!";
+        Time.timeScale = 0;
+        gameWon = true;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         if (timerActive)
         {
@@ -28,6 +41,11 @@ public class TimerScript : MonoBehaviour
         else
         {
             timerText.text = "";
+        }
+        if (ItemInteraction.itemsFound == numItems)
+        {
+
+            Invoke("wait", 0.5f);
         }
     }
 
